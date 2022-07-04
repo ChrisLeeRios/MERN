25b9eddad78518c6b714543e86d135c9013bbcdc
Christopher Rios <chris12k9@yahoo.com>
2022-07-04 13:08:48 -0700

    added super ninja

--- Ninja/ninja.js
+++ Ninja/ninja.js
@@ -21,8 +21,27 @@ class Ninja {
 }
 
 
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+class Sensei extends Ninja{
+    constructor(name){
+        super(name)
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log(`${this.name} Speaks : You will stop caring so much about what people think of you, when you realize how seldom they do.\n`)
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.showStats();
+superSensei.speakWisdom();
+superSensei.showStats();
+
+// const ninja1 = new Ninja("Hyabusa");
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+// ninja1.showStats();
