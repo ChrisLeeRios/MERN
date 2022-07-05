73b57ff13822f16a2b857d051676bf6758ecea1a
Christopher Rios <chris12k9@yahoo.com>
2022-07-05 15:26:26 -0700

    added first jsx

---
+++ firstjsx/src/components/NewComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class NewComponent extends Component {
+    render() {
+        return (
+            <>
+                <h1>HELLO DOJO</h1>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li> Learn React</li>
+                    <li> Climb Mt. Everest</li>
+                    <li> Run A Marathon</li>
+                    <li> Feed The Dogs</li>
+                </ul>
+            </>
+        );
+    }
+}
+
+export default NewComponent;
