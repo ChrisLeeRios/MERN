a8152010d7b161b9efe2c33bf0402109fea8f551
Christopher Rios <chris12k9@yahoo.com>
2022-07-06 14:59:34 -0700

    added functional components

--- propitup/src/components/PersonCardComponent.js
+++ propitup/src/components/PersonCardComponent.js
@@ -1,27 +1,43 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 import classes from './PersonCardComponent.module.css'
 
-class PersonCard extends Component {
-    constructor(props){
-        super(props);
-        this.state = {age : this.props.age}
-    }
-    // could have done thid login in a function like so 
-    // changeAge = () => {this.setState({ age: this.state.age +1 });};
-    // and the set the button onClick=(this.chanegAge)
-    render() {
-        return (
-            <div className={classes.card}>
-                <div className={classes.content}>
-                    <h1>{this.props.lastName}, {this.props.firstName} </h1>
-                    <p>Age: {this.state.age}</p>
-                    <p>Hair Color: {this.props.hairColor}</p>
-                    <button onClick={ () => { this.setState({ age: this.state.age +1 }) } }>
-                        BirthDay Button For {this.props.firstName} {this.props.lastName} </button>
-                </div>
-            </div>
-        );
-    }
-}
+
+// class PersonCard extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {age : this.props.age}
+//     }
+//     // could have done thid login in a function like so 
+//     // changeAge = () => {this.setState({ age: this.state.age +1 });};
+//     // and the set the button onClick=(this.chanegAge)
+//     render() {
+//         return (
+//             <div className={classes.card}>
+//                 <div className={classes.content}>
+//                     <h1>{this.props.lastName}, {this.props.firstName} </h1>
+//                     <p>Age: {this.state.age}</p>
+//                     <p>Hair Color: {this.props.hairColor}</p>
+//                     <button onClick={ () => { this.setState({ age: this.state.age +1 }) } }>
+//                         BirthDay Button For {this.props.firstName} {this.props.lastName} </button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+const PersonCard = props => {
+                return (
+                    <div className={classes.card}>
+                        <div className={classes.content}>
+                            <h1>{props.lastName}, {props.firstName} </h1>
+                            <p>Age: {props.age}</p>
+                            <p>Hair Color: {props.hairColor}</p>
+                            <button onClick={ 1 }>
+                                BirthDay Button For {props.firstName} {props.lastName} </button>
+                        </div>
+                    </div>
+                );
+        }
+
 
 export default PersonCard
