6b15ad403af177d47e23a70523ba9f97003635bb
Christopher Rios <chris12k9@yahoo.com>
2022-07-05 18:16:37 -0700

    added age up button

--- propitup/src/components/PersonCardComponent.js
+++ propitup/src/components/PersonCardComponent.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import classes from './PersonCardComponent.module.css'
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {age : this.props.age}
+    }
     render() {
         return (
             <div className={classes.card}>
                 <div className={classes.content}>
                     <h1>{this.props.lastName}, {this.props.firstName} </h1>
-                    <p>Age: {this.props.age}</p>
+                    <p>Age: {this.state.age}</p>
                     <p>Hair Color: {this.props.hairColor}</p>
+                    <button onClick={ () => { this.setState({ age: this.state.age +1 }) } }>
+                        BirthDay Button For {this.props.firstName} {this.props.lastName} </button>
                 </div>
             </div>
         );
