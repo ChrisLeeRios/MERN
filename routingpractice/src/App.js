9905eeb1d467ec580911396e5fa9e3b7626e6728
Christopher Rios <chris12k9@yahoo.com>
2022-07-11 22:15:45 -0700

    added example of LINK TO

--- routingpractice/src/App.js
+++ routingpractice/src/App.js
@@ -24,6 +24,7 @@ const TheInput = (props) => {
 const TheInputs = (props) => {
   const { input, color, bgcolor } = useParams();
 
+
   if (isNaN(+ input) === false) {
     return (
       <h1 style={{ color: color, backgroundColor: bgcolor }}>The number is: {input} </h1>
@@ -45,5 +46,30 @@ function App() {
     </div>
   );
 }
+
+// const Location = (props) => { 
+//   const { kity } = useParams(); // matches our :city in our Routes
+
+//   return (
+//     <h1>Welcome to { kity }!</h1>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <p>
+//         <Link to="/seattle">Seattle</Link>
+//         |
+//         <Link to="/chicago">Chicago</Link>
+//         |
+//         <Link to="/burbank">Burbank</Link>
+//       </p>
+//       <Routes>
+//         <Route path="/:kity" element={<Location />} />
+//       </Routes>
+//     </div>
+//   );
+// }
 
 export default App;
