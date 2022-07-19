08e342a5d92ab26fca3f4d2533c4b2bd31ec4fb0
Christopher Rios <chris12k9@yahoo.com>
2022-07-19 10:09:11 -0700

    added comment

--- ProductManager/server.js
+++ ProductManager/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 require('./server/config/mongoose.config'); 
 
+// Make sure these lines are above any app.get app.post lines
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
