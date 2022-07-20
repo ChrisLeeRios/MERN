f4e4e03b67600a5b5f89a73807a9304dc1c33c0f
Christopher Rios <chris12k9@yahoo.com>
2022-07-19 23:03:58 -0700

    init

--- ExamMern/server/models/exam.model.js
+++ ExamMern/server/models/exam.model.js
@@ -2,17 +2,32 @@ const mongoose = require('mongoose');
 
 // Change all lines below to match the models for current project
 const ExamSchema = new mongoose.Schema({
-    name: { type: String,
-    required: [true, "Name is required!"],
-    minlength: [2, "Name must be atleast 2 characters long!"] },
-
-    price: { type: Number,
-    required: [true, "Price is required!"],
-    min: [0, "Can't be less than free!"],
-    max: [1000, "Can't be more than 1000!"] },
-
-    imgUrl: { type: String,
+    title: {
+        type: String,
+        required: [true, "Title is required!"],
+        minlength: [2, "Title must be atleast 2 characters long!"],
+        maxlength: [20, "Title must be at most 20 characters long!"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required!"],
+        min: [0, "Can't be less than free!"],
+        max: [10000, "Can't be more than 10k!"]
+    },
+    description: {
+        type: String,
         required: [true, "Description is required!"],
-        minlength: [2, "Description must be atleast 2 characters long!"] }
+        minlength: [2, "Description must be atleast 2 characters long!"]
+    },
+    imgUrl: {
+        type: String,
+        required: [true, "Image is required!"],
+    },
+    expirationDate: {
+        type: Date
+    },
+    isExpensive: {
+        type: Boolean
+    },
 }, { timestamps: true });
 module.exports.Exam = mongoose.model('Exam', ExamSchema);
